4c6788618c0145d1435e708bbc186d96422769cd
joeshithaapandian <154114850+joeshithaapandian@users.noreply.github.com>
2024-12-04 08:00:16 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,24 +1,10 @@
-# [START gae_python38_app]
-# [START gae_python3_app]
 from flask import Flask
 
-# If entrypoint is not defined in app.yaml, App Engine will look for an app
-# called app in main.py.
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route("/")
-def hello():
-    """Return a friendly HTTP greeting.
+@app.route('/')
+def welcome():
+    return "Welcome to Google App Engine!"
 
-    Returns:
-        A string with the words 'Hello World!'.
-    """
-    return "Welcome!"
-
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding entrypoint to app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=8080)
